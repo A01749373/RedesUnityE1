4b28f5b6a9f5a8cdacc2a91be7e66671868d4cbf
a01750147 <A01750147@itesm.mx>
2021-04-09 23:48:33 -0500

    CambiosUnity

--- Demo2D_Plataforma/Assets/Scripts/Enemigo.cs
+++ Demo2D_Plataforma/Assets/Scripts/Enemigo.cs
@@ -1,34 +1,61 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+using UnityEngine.Networking;
+
 /*
  * Detecta la colisión del enemigo con el personaje
- * Autora: Amy Murakami Tsutsumi
+ * Autor: Equipo 1
  */
 public class Enemigo : MonoBehaviour
 {
-    //Referencia al Audio Source
     public AudioSource efectoEnemigo;
     public AudioSource efectoMuere;
+    public static String tiempoFinal;
+    public static Enemigo instance;
 
-    public void OnTriggerEnter2D(Collider2D other)
+    public IEnumerator OnTriggerEnter2D(Collider2D other)
     {
         if (other.gameObject.CompareTag("Player"))
         {
-            //Reproducir un efecto de sonido
+            //Reproducir el efecto
             efectoEnemigo.Play();
-
-            //Descontar una vida 
-            SaludPersonaje.instance.vidas--; //Se disminuye en uno la variable vidas
-            //Actualizar los 'corazones'
+            //Descontar una vida
+            SaludPersonaje.instance.vidas--;
+            //Actualizar los corazones
             HUD.instance.ActualizarVidas();
+
             if (SaludPersonaje.instance.vidas == 0)
             {
                 efectoMuere.Play();
                 Destroy(other.gameObject, 5f);
-                //SceneManager.LoadScene("EscenaMenu"); //Pierde, regresa al menú
+                //SceneManager.LoadScene("EscenaMenu"); //Pierde, regresa al menu
+
+                tiempoFinal = System.DateTime.Now.TimeOfDay.ToString(); //Declaramos la hora final en el momento que pierde las 3 vidas
+                String TiempoInicio2 = Red.tiempoInicio; //Traemos del archivo Red el tiempo inicio
+
+                WWWForm forma2 = new WWWForm();
+
+                forma2.AddField("TiempoInicio", TiempoInicio2);
+                forma2.AddField("TiempoFinal", tiempoFinal);
+                forma2.AddField("TiempoUsuario", Red.nombre);
+
+                UnityWebRequest request = UnityWebRequest.Post("http://Localhost:8080/AgregarTiempo", forma2);
+                yield return request.SendWebRequest();
+                if (request.result == UnityWebRequest.Result.Success)  //200 OK
+                {
+                    string textoPlano = request.downloadHandler.text;  //Datos descargados de la red
+                    print(textoPlano);
+                }
+                else
+                {
+                    print("Error en la descarga: " + request.responseCode.ToString());
+                }
             }
         }
     }
 }
+
--- Demo2D_Plataforma/Assets/Scripts/Red.cs
+++ Demo2D_Plataforma/Assets/Scripts/Red.cs
@@ -1,151 +1,62 @@
 using System;
 using System.Collections;
 using System.Collections.Generic;
-using Newtonsoft.Json;
 using UnityEngine;
-using UnityEngine.Networking; //Para red, UnityWebRequest
 using UnityEngine.UI;
+using UnityEngine.Networking;
+using UnityEngine.SceneManagement;
+//using Newtonsoft.Json;
 
 /*
  * Muestra el uso de UnityWebRequest para comunicarse con un servidor en la red
- * Autor:
- * Andrea Vianey Díaz Álvarez
- * Liam Garay Monroy
- * Amy Murakami Tsutsumi
- * Jorge Chávez Badillo
- * Ariadna Jocelyn Guzmán
+ * Autor: Equipo 1
  */
+
 public class Red : MonoBehaviour
 {
-    //Variable para desplegar la informacion
+    //Para desplegar la información
     public Text resultado;
+
+    public static Red instance;
+    public static String tiempoInicio;
+
+    //Campos con la información nombre y puntos
     public Text textoNombre;
-    public Text textoPuntos;
-
-    public struct DatosUsuario
-    {
-        public string nombre;
-        public int puntos;
-    }
-    //Encapsular los datos JSON
-    public DatosUsuario datos;
-    
-    //ESCRIBIR JSON
-    public void EscribirJSON()
+    public Text textoContraseña;
+    public static String nombre;
+    //Escribir
+    public void EscribirTextoPlano()     //Botón
     {
         //Concurrente
-        StartCoroutine(SubirJSON()); //Paralelo
-    }
-    
-    //Parte se ejecuta en paraalelo
-    private IEnumerator SubirJSON()
-    {
-        datos.nombre = textoNombre.text;
-        datos.puntos= Int32.Parse(textoPuntos.text);
-        print(JsonUtility.ToJson(datos));
-        //Encapsular los datos que se suben a red con POST
-        WWWForm forma = new WWWForm();
-        forma.AddField("datosJSON", JsonUtility.ToJson(datos));
-        UnityWebRequest request = UnityWebRequest.Post("http://localhost:8080/recibeJSON",forma); //http://189.139.248.236/unity/recibeJSON.php
-        yield return request.SendWebRequest(); //Regresa, ejecuta, espera...
-        // ... ya regresó por que termino de ejecutar SendWebRequest()
-        if (request.result == UnityWebRequest.Result.Success)  //200 
-        {
-            string textoPlano = request.downloadHandler.text; //Datos descargados de la red
-            resultado.text = textoPlano;
-        }
-        else
-        {
-            resultado.text = "Error en la descarga: " + request.responseCode.ToString();
-        }
-        
-    }
-    public void LeerJSON() //Boton
-    {
-        //Concurrente
-        StartCoroutine(DescargarJSON()); //'Paralelo'
-
-    }
-    
-    //Parte de este codigo se ejecuta en 'Paralelo'
-    private IEnumerator DescargarJSON()
-    {
-        UnityWebRequest request = UnityWebRequest.Get("http://localhost:8080/generaJSON"); //http://189.139.248.236/unity/generaJSON.php
-        yield return request.SendWebRequest(); //Regresa, ejecuta, espera...
-        // ... ya regresó por que termino de ejecutar SendWebRequest()
-        if (request.result == UnityWebRequest.Result.Success)  //200 
-        {
-            string textoPlano = request.downloadHandler.text; //Datos descargados de la red
-            resultado.text = textoPlano;
-
-            Dictionary<string, string> datos =
-                JsonConvert.DeserializeObject<Dictionary<string, string>>(textoPlano);
-            foreach (var dato in datos)
-            {
-                print(dato.Key + " : " + dato.Value);
-            }
-            {
-                
-            }
-        }
-        else
-        {
-            resultado.text = "Error en la descarga: " + request.responseCode.ToString();
-        }
+        StartCoroutine(SubirTextoPlano());
     }
 
-
-    //ESCRIBIR
-    public void EscribirTextoPlano()
-    {
-        //Concurrente
-        StartCoroutine(SubirTextoPlano()); //Paralelo
-    }
-    
-    //Parte se ejecuta en paraalelo
     private IEnumerator SubirTextoPlano()
     {
-        //Encapsular los datos que se suben a red con POST
+        //Encapsular los datos que se suben a la red con el método POST
         WWWForm forma = new WWWForm();
-        forma.AddField("nombre", textoNombre.text);
-        forma.AddField("puntos", textoPuntos.text);
-        UnityWebRequest request = UnityWebRequest.Post("http://localhost:8080/recibeTextoPlano",forma);  //http://189.139.248.236/unity/recibeTextoPlano.php
-        yield return request.SendWebRequest(); //Regresa, ejecuta, espera...
-        // ... ya regresó por que termino de ejecutar SendWebRequest()
-        if (request.result == UnityWebRequest.Result.Success)  //200 
-        {
-            string textoPlano = request.downloadHandler.text; //Datos descargados de la red
-            resultado.text = textoPlano;
-        }
-        else
-        {
-            resultado.text = "Error en la descarga: " + request.responseCode.ToString();
-        }
-        
-    }
 
-    public void LeerTextoPlano() //Boton
-    {
-        //Concurrente
-        StartCoroutine(DescargarTextoPlano()); //'Paralelo'
+        forma.AddField("usuarioUsuarioo", textoNombre.text);
+        forma.AddField("passwordUsuarioo", textoContraseña.text);
 
-    }
-    
-    //Parte de este codigo se ejecuta en 'Paralelo'
-    private IEnumerator DescargarTextoPlano()
-    {
-        UnityWebRequest request = UnityWebRequest.Get("http://localhost:8080/texto"); //http://189.139.248.236/unity/texto.txt
-        yield return request.SendWebRequest(); //Regresa, ejecuta, espera...
-        // ... ya regresó por que termino de ejecutar SendWebRequest()
-        if (request.result == UnityWebRequest.Result.Success)  //200 
+        UnityWebRequest request = UnityWebRequest.Post("http://Localhost:8080/BuscarUsuario", forma); //
+        yield return request.SendWebRequest();   //Regresa, ejecuta, espera...
+        //...ya regresó a la línea 27 (terminó de ejecutar SendWebRequest())
+
+        if (request.result == UnityWebRequest.Result.Success)  //200 OK
         {
-            string textoPlano = request.downloadHandler.text; //Datos descargados de la red
+            string textoPlano = request.downloadHandler.text;  //Datos descargados de la red
             resultado.text = textoPlano;
+            if (textoPlano == "")
+            {
+                SceneManager.LoadScene("EscenaMapa");
+                tiempoInicio = System.DateTime.Now.TimeOfDay.ToString();
+                nombre = textoNombre.text;
+            }
         }
         else
         {
             resultado.text = "Error en la descarga: " + request.responseCode.ToString();
         }
     }
-
 }
